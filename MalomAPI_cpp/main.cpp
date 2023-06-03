439ca82d8e9bbd5777328eb142ca794bd50d4239
Calcitem <calcitem@outlook.com>
2023-06-04 00:01:40 +0800

    Add main.cpp

--- MalomAPI_cpp/main.cpp
+++ MalomAPI_cpp/main.cpp
@@ -1,3 +1,27 @@
-int main() {
+// You have to set the working directory to the directory of the database.
+
+// #define no_init_all // Was needed only in VS 2017
+
+#define USE_DEPRECATED_CLR_API_WITHOUT_WARNING
+
+#include <cstdio>
+#include <sstream>
+#include <string>
+
+#include "MalomSolutionAccess.h"
+
+int main()
+{
+    int res = MalomSolutionAccess::getBestMove(1, 2, 8, 8, 0, false); // Correct output: 16384
+    // int res = GetBestMove(1 + 2 + 4, 8 + 16 + 32, 100, 0, 0, false); // tests exception
+    // int res = GetBestMove(1 + 2 + 4, 1 + 8 + 16 + 32, 0, 0, 0, false); // tests exception
+    // int res = GetBestMove(1 + 2 + 4, 8 + 16 + 32, 0, 0, 0, true); // Correct output: any of 8, 16, 32
+
+    printf("GetBestMove result: %d\n", res);
+
+#ifdef _WIN32
+    system("pause");
+#endif 
+
     return 0;
 }
